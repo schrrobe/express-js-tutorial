3892b94f6b2a9a39b4b38a492fbd7730b27f5776
Robert Schreiner <robsch@stagedates.com>
2025-12-07 13:47:57 +0100

    Improves code readability with formatting
    Addresses code style issues by formatting the code
    for improved readability and maintainability.

--- src/index.mjs
+++ src/index.mjs
@@ -6,21 +6,21 @@ dotenv.config()
 const app = express()
 
 const mockUsers = [
-        {
-            id: 1,
-            username: 'John Doe',
-            displayName: 'John Doe'
-        },
-        {
-            id: 2,
-            username: 'Max Mustermann',
-            displayName: 'MM'
-        },
-        {
-            id: 3,
-            username: 'James Bond',
-            displayName: 'JB 007'
-        }
+  {
+    id: 1,
+    username: 'John Doe',
+    displayName: 'John Doe'
+  },
+  {
+    id: 2,
+    username: 'Max Mustermann',
+    displayName: 'MM'
+  },
+  {
+    id: 3,
+    username: 'James Bond',
+    displayName: 'JB 007'
+  }
 ]
 
 app.get('/', (request, response) => {
@@ -33,18 +33,18 @@ app.get('/api/users', (request, response) => {
 })
 
 app.get('/api/users/:id', (request, response) => {
-    console.log(request.params);
-    const parsedId = parseInt(request.params.id);
-    console.log(parsedId);
-    if(isNaN(parsedId) || parsedId === null) {
-        return response.status(400).send({msg: 'Wrong Id, Bad request'})
-    }
+  console.log(request.params)
+  const parsedId = parseInt(request.params.id)
+  console.log(parsedId)
+  if (isNaN(parsedId) || parsedId === null) {
+    return response.status(400).send({ msg: 'Wrong Id, Bad request' })
+  }
 
-    const findUser = mockUsers.find((user) => user.id === parsedId);
-    if (!findUser) {
-        return response.sendStatus(400).send({msg: 'Wrong Id, Bad request'})
-    }
-    return response.status(200).send(findUser)
+  const findUser = mockUsers.find((user) => user.id === parsedId)
+  if (!findUser) {
+    return response.sendStatus(400).send({ msg: 'Wrong Id, Bad request' })
+  }
+  return response.status(200).send(findUser)
 })
 
 app.get('/api/products', (request, response) => {
