0bc1a6be2e339210e0219379eb3bb3b2495cc38d
Robert Schreiner <robsch@stagedates.com>
2025-12-07 13:04:12 +0100

    feat: using route parameters
    Implements a GET endpoint to fetch all users and
    a GET endpoint to retrieve a specific user by ID.
    Returns a 400 status if an invalid or non-existent
    ID is provided.

--- src/index.mjs
+++ src/index.mjs
@@ -5,29 +5,46 @@ dotenv.config()
 
 const app = express()
 
+const mockUsers = [
+        {
+            id: 1,
+            username: 'John Doe',
+            displayName: 'John Doe'
+        },
+        {
+            id: 2,
+            username: 'Max Mustermann',
+            displayName: 'MM'
+        },
+        {
+            id: 3,
+            username: 'James Bond',
+            displayName: 'JB 007'
+        }
+]
+
 app.get('/', (request, response) => {
   response.status(201).send({ msg: 'Hello World!' })
   // response.send('Hello World!');
 })
 
 app.get('/api/users', (request, response) => {
-  response.status(201).send([
-    {
-      id: 1,
-      username: 'John Doe',
-      displayName: 'John Doe'
-    },
-    {
-      id: 2,
-      username: 'Max Mustermann',
-      displayName: 'MM'
-    },
-    {
-      id: 3,
-      username: 'James Bond',
-      displayName: 'JB 007'
+  response.status(201).send(mockUsers)
+})
+
+app.get('/api/users/:id', (request, response) => {
+    console.log(request.params);
+    const parsedId = parseInt(request.params.id);
+    console.log(parsedId);
+    if(isNaN(parsedId) || parsedId === null) {
+        return response.status(400).send({msg: 'Wrong Id, Bad request'})
     }
-  ])
+
+    const findUser = mockUsers.find((user) => user.id === parsedId);
+    if (!findUser) {
+        return response.sendStatus(400).send({msg: 'Wrong Id, Bad request'})
+    }
+    return response.status(200).send(findUser)
 })
 
 app.get('/api/products', (request, response) => {
