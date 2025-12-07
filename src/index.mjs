f4ad7219f6c783982b00e5801fc5d11fcb6a5418
Robert Schreiner <robsch@stagedates.com>
2025-12-07 12:39:47 +0100

    feat: add basic get API endpoints
    Initializes the project with basic get API endpoints for users and products.
    
    Sets up Express server and defines routes for
    retrieving user and product data. Includes a default route.

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,44 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT ?? 3000;
+app.get('/', (request, response) => {
+  response.status(201).send({ msg: 'Hello World!' })
+  // response.send('Hello World!');
+})
+
+app.get('/api/users', (request, response) => {
+  response.status(201).send([
+    {
+      id: 1,
+      username: 'John Doe',
+      displayName: 'John Doe'
+    },
+    {
+      id: 2,
+      username: 'Max Mustermann',
+      displayName: 'MM'
+    },
+    {
+      id: 3,
+      username: 'James Bond',
+      displayName: 'JB 007'
+    }
+  ])
+})
+
+app.get('/api/products', (request, response) => {
+  response.status(201).send([
+    { id: 123, name: 'chicken', price: 12.99 },
+    { id: 321, name: 'cat', price: 22.33 },
+    { id: 231, name: 'dog', price: 6.21 }
+  ])
+})
+
+const PORT = process.env.PORT ?? 3000
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+  console.log(`Listening on port ${PORT}`)
+})
